f2396c8a656410686715063a1e52a42b14e9ed1d
Santiago Lezica <slezica89@gmail.com>
2017-09-07 17:11:35 -0300

    Drawing chart!

--- app/src/main/java/com/example/slezica/dailyself/ui/view/PursuitItem.java
+++ app/src/main/java/com/example/slezica/dailyself/ui/view/PursuitItem.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -15,8 +16,13 @@ import com.example.slezica.dailyself.model.Pursuit;
 import com.example.slezica.dailyself.model.PursuitEntry;
 import com.example.slezica.dailyself.utils.Callback1;
 import com.example.slezica.dailyself.utils.RichText;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import org.threeten.bp.ZonedDateTime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PursuitItem extends BaseView {
@@ -27,8 +33,8 @@ public class PursuitItem extends BaseView {
     @BindView(R.id.latest_entry)
     TextView latestEntry;
 
-//    @BindView(R.id.chart)
-//    LineChart chart;
+    @BindView(R.id.chart)
+    LineChart chart;
 
     Pursuit pursuit;
     Callback1<Pursuit> onAddEntryClick;
@@ -64,7 +70,35 @@ public class PursuitItem extends BaseView {
         if (entries.size() == 0) return;
         if (! entries.get(0).getPursuit().equals(pursuit)) return;
 
+        setChartEntries(entries);
         setLatestEntry(entries.get(entries.size() - 1));
+    }
+
+    private void setChartEntries(List<PursuitEntry> entries) {
+        final List<Entry> chartEntries = new ArrayList<Entry>();
+
+        int index = 0;
+        for (PursuitEntry entry: entries) {
+            index++;
+            chartEntries.add(new Entry(index, index));
+        }
+
+        final LineDataSet dataSet = new LineDataSet(chartEntries, "Scores");
+        dataSet.setLineWidth(3f);
+
+        final LineData data = new LineData(dataSet);
+        data.setDrawValues(false);
+
+        chart.setData(data);
+        chart.setDescription(null);
+        chart.setDrawGridBackground(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+
+        chart.setVisibility(View.VISIBLE);
+        chart.invalidate();
     }
 
     private void setLatestEntry(PursuitEntry entry) {
